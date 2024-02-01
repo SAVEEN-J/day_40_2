665bcf301644890b9dfb9206ea39162f0edfc860
SAVEEN-J <saveenj1996@gmail.com>
2024-02-01 09:42:14 +0530

    final

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 
-
+require("dotenv").config();
 const mongose = require('mongoose');
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const mongoose = require("mongoose");
 
-// const PORT=process.env.ATLAS_URI;
 
-const url =`mongodb+srv://saveen:saveen@cluster0.sjce8ng.mongodb.net/DAY_40First?retryWrites=true&w=majority`
-mongose.connect(url)
+app.use(express.json());
+ const url=process.env.ATLAS_URI;
+
+// const url =`mongodb+srv://saveen:saveen@cluster0.sjce8ng.mongodb.net/DAY_40First?retryWrites=true&w=majority`
+ mongose.connect(url)
 .then(()=>{
     console.log("Connected to Mongo DB");
     // mongose.connection.close(); it is close the connection when the mongo db is connected
@@ -27,7 +28,9 @@ const noteSchema =new mongose.Schema({
 });
 
 //create a model
-const Note =mongose.model('Note',noteSchema,'Day41')
+const Note =mongose.model('Note',noteSchema,'CollectionName')
+console.log(Note);
+
 
 app.get('/',(req,res,next)=>{
   res.send("<h1>Mongo Configration and api </h1>");
@@ -37,21 +40,73 @@ app.get("/api/notes", (req, res) => {
   Note.find({}, {}).then((notes) => {
     res.status(200).json(notes);
   });
-});
+}); 
 
-app.post("/api/notes", (req, res) => {
+//store data in backend
+//app.use(express.json());to post  json data
+app.post('/api/notes', (req, res) => {
+  // console.log(req.body);
   //preparing object to store in collection
   const note = new Note(req.body);
-  note.save().then(() => {
+  // console.log(note);
+  note.save()
+      .then(() => {
     res.status(201).json({ message: "post request completed" });
   });
 });
 
+// getting data of particular id for update
 
+app.get("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
+  Note.findById(id).then((note) => {
+    if (note) {
+      res.status(200).json(note);
+    } else {
+      res.status(404).json({ message: "id not found" });
+    }
+  });
+});
 
+// delete data of particular id
 
+app.delete("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
+  Note.findByIdAndDelete(id).then((deletedNote) => {
+    if (deletedNote) {
+      res.status(200).json({ message: "Deleted sucessfully" });
+    } else {
+      res.status(404).json({ message: "id not found" });
+    }
+  });
+});
+// put data of particular id update
 
+app.put("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
+  const updatedNote = req.body;
+  Note.findByIdAndUpdate(id, updatedNote).then((updatedNote) => {
+    if (updatedNote) {
+      res.status(200).json({ message: "updated" });
+    } else {
+      res.status(404).json({ message: "id not found" });
+    }
+  });
+});
 
+ // patching data of particular id update
+
+app.patch("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
+  const updatedNote = req.body;
+  Note.findByIdAndUpdate(id, updatedNote).then((updatedNote) => {
+    if (updatedNote) {
+      res.status(200).json({ message: "updated" });
+    } else {
+      res.status(404).json({ message: "id not found" });
+    }
+  });
+});
 
 app.listen(8080, () => {
     console.log("server running in port 8080");
